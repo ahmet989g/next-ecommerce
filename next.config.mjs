ead8f01573be87dacb17d8179ec3ddfa8f9f4acb
ahmet989g <ahmet.erden98@gmail.com>
2024-07-27 00:41:20 +0300

    categories fetching added

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns:[
-            {
-                protocol: 'https',
-                hostname: "fastly.picsum.photos",
-            },
-            {
-                protocol: 'https',
-                hostname: "images.pexels.com",
-            },
-            {
-                protocol: 'https',
-                hostname: "static.wixstatic.com",
-            },
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fastly.picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.wixstatic.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.fera.ai',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -9,12 +9,42 @@ const PRODUCT_PER_PAGE = 20;
 type ProductListProps = {
   categoryId?: string;
   limit?: number;
+  searchParams?: any;
 }
 
-const ProductList = async ({ categoryId, limit }: ProductListProps) => {
+const ProductList = async ({ categoryId, limit, searchParams }: ProductListProps) => {
 
   const wixClient = await wixClientServer();
-  const res = await wixClient.products.queryProducts().eq("collectionIds", String(categoryId)).limit(limit || PRODUCT_PER_PAGE).find();
+  const productQuery = wixClient.products
+    .queryProducts()
+    .startsWith("name", searchParams?.name || "")
+    .eq("collectionIds", String(categoryId))
+    .hasSome(
+      "productType",
+      searchParams?.type ? [searchParams.type] : ["physical", "digital"]
+    )
+    .gt("priceData.price", searchParams?.min || 0)
+    .lt("priceData.price", searchParams?.max || 999999)
+    .limit(limit || PRODUCT_PER_PAGE)
+    .skip(
+      searchParams?.page
+        ? parseInt(searchParams.page) * (limit || PRODUCT_PER_PAGE)
+        : 0
+    );
+  // .find();
+
+  if (searchParams?.sort) {
+    const [sortType, sortBy] = searchParams.sort.split(" ");
+
+    if (sortType === "asc") {
+      productQuery.ascending(sortBy);
+    }
+    if (sortType === "desc") {
+      productQuery.descending(sortBy);
+    }
+  }
+
+  const res = await productQuery.find();
 
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
