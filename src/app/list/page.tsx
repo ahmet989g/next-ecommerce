e5d083509d944473c0356afa979a553ca43b3696
ahmet989g <ahmet.erden98@gmail.com>
2024-08-01 00:29:29 +0300

    Page pagination added

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -43,6 +43,7 @@ const ListPage = async ({ searchParams }: ListPageProps) => {
             cat.collection?._id || "00000000-000000-000000-000000000001"
           }
           searchParams={searchParams}
+          isPaginationActive={true}
         />
       </Suspense>
     </div>
